6e6c0317806f22069ae25111fce77b79c416ec21
umi <ianlumanog.iml@gmail.com>
2022-12-25 15:44:48 +0800

    Guide for the development
    I listed here the guides all of you can use so we can start developing.

---
+++ main.py
@@ -0,0 +1,50 @@
+# Welcome !
+# Use this comments as guide to our project
+
+
+# START
+
+
+# WELCOME SCREEN
+
+
+
+
+
+# AFTER LOADING IS DONE
+# ASK USER NAME
+
+
+
+
+
+
+
+# GAME MENU
+
+
+
+
+
+
+# SONG SELECTION
+
+
+
+
+# TOTAL SONGLIST PLAYER CHOSE
+
+
+
+
+# START OF ROUND
+
+
+
+
+# SONGLIST IS EMPTY = GAME OVER
+
+
+
+
+# EXIT HERE
