88d6644b8b0c4ce3e156b2ea0fd9c9787409be64
umi <ianlumanog.iml@gmail.com>
2023-01-03 11:27:01 +0800

    Updated the guide

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 # Welcome !
 # Use this comments as guide to our project
-<<<<<<< Updated upstream
 # make sure you're on a development branch!
 
-=======
 
-print("hello")
->>>>>>> Stashed changes
 # START
 
-print("Hi! I'm Irish, Nice to meet you")
-# WELCOME SCREEN
-<<<<<<< Updated upstream
-#ako si umi
-# haist
 
-# changes here
-=======
-print("hello world")
->>>>>>> Stashed changes
+# WELCOME SCREEN
 
 
 
