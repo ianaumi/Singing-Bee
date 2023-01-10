136af045065bf6e1755f3f481505521f8a7cc438
Grule07 <118157420+Grule07@users.noreply.github.com>
2023-01-10 20:07:19 +0800

    updated start round

--- src/start_round.py
+++ src/start_round.py
@@ -1,14 +1,30 @@
 import utils as util
 import displays as display
-import songList.json
-import main as mains
 
 
-def song_info(value):
-    return util.get_song_list_file[songList.year_choice[0]][songList.song_choice[0]][value]
+
+
+def song_info(year_choice, song_choice, value):
+    songList = util.get_song_list_file()
+    return songList[year_choice[0]][song_choice[0]][value]
 
 
 #FIXME parameter name
+
+
+def play_again(player_choice):
+    while True:
+        player_choice = input("Do you want to play again? Y/N : ")
+        if player_choice.upper() in util.options(["Y", "N"]):
+            if player_choice.upper() == "Y":
+                return
+
+            elif player_choice.upper() == "N":
+                return display.quit_screen(player_name)
+        else:
+            print("Invalid Option")
+
+
 def is_empty(empty):
     if not empty:
         return True
@@ -20,11 +36,15 @@ def round_start():
     global player_points
     global hints
     global player_choice
+    global player_name
+    global year_choice
+    global song_choice
     while True:
+        hints = 3
 
-        print(songList.song_choice[0])
-        print(song_info("lyrics"))
-        print(song_info("choices"))
+        print(song_choice[0])
+        print(song_info(year_choice[0], song_choice[0], "lyrics"))
+        print(song_info(year_choice[0], song_choice[0], "choice"))
 
         print("Total hints available: ", hints)
         while True:
@@ -35,17 +55,17 @@ def round_start():
                 if player_choice.upper() == "H":
                     if hints > 0:
                         if not hint_used:
-                            print(song_info("hint"))
+                            print(song_info(year_choice[0], song_choice[0], "hint"))
                             hint_used = True
                             hints = hints - 1
                             while True:
                                 player_choice = input("What is your choice: ")
-                                if player_choice.upper() in song_info("hint"):
+                                if player_choice.upper() in song_info(year_choice[0], song_choice[0], "hint"):
                                     break
                                 else:
                                     print("Invalid Option")
 
-                if player_choice.upper() == (song_info("answer")):
+                if player_choice.upper() == (song_info(year_choice[0], song_choice[0], "answer")):
                     print("Correct!")
 
                     if hint_used:
@@ -63,16 +83,9 @@ def round_start():
             else:
                 print("Invalid Option")
 
-        songList.song_choice.pop(0)
-        songList.year_choice.pop(0)
+        song_choice.pop(0)
+        year_choice.pop(0)
 
-        if is_empty(songList.year_choice):
-            while True:
-                player_choice = input("Do you want to play again? Y/N : ")
-                if player_choice.upper() in util.options(["Y", "N"]):
-                    if player_choice.upper() == "Y":
-                        return mains.main()
-                    elif player_choice.upper() == "N":
-                        return display.quit_screen()
-                else:
-                    print("Invalid Option")
+        if is_empty(year_choice):
+
+            play_again(player_choice)
