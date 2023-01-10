a9281276813f6c9038419b89f96c12907a164d6b
umi <ianlumanog.iml@gmail.com>
2023-01-11 02:33:21 +0800

    added skip song function

--- src/start_round.py
+++ src/start_round.py
@@ -15,38 +15,67 @@ def ask_play_again(player_name):
         display.quit_screen(player_name)
 
 def player_status(player_name,player_hints,player_points):
-    display.print_position(2, 5, f"Player:{player_name}")
-    display.print_position(0, 5, f"Hints:{player_hints}")
-    display.print_position(0, 5, f"Honeys:{player_points}")
+    player = player_name
+    hints = player_hints
+    honey = player_points
+    #FIXME
+    display.print_position(2, 5, f"| Player: {player:^{20}} |")
+    display.print_position(0, 5, f"| Hints: {hints:^{20}} |")
+    display.print_position(0, 5, f"| Honeys: {honey:^{20}} |")
 
 def run_start_round(player_name):
+    display.loading_screen()
     player_points = 0 # stores the points of the user once the round started
     player_hints = 3
-    while True:
-        display.header_text(f"# Song playing - {song_choice[0]}")
-        player_status(player_name,player_hints,player_points)
-        display.print_position(2,10,song_info("lyrics"))
-        print(song_info("choices"))
+    player_skipped_song = False
 
-        print("Hints", player_hints)
+    # # >>>>>>>>>>>>>>>>>>>>>TESTING BLOCK <<<<<<<<<<<<<<<<<<<<<
+    # while True:
+    #     print(song_list["2020s"]["About you - 1975"]["lyrics"])
+    #     choice = input()
+    #     if choice == 'A':
+    #         exit()
+
+
+    while True:
+        display.header_text(f"# Song playing - {song_choice[0]}\n\n ## Remaining songs: {len(song_choice)}")
+
+        #FIXME alignment
+        player_status(player_name,player_hints,player_points)
+        display.print_position(2,5,song_info("lyrics"))
+
+        if not player_skipped_song:
+            util.play_sound(song_info("sound"))
+            display.print_position(3,23,"press enter to continue")
+            util.get_input_position(1, 23)
+            util.stop_sound()
+            player_skipped_song = True
+            display.clear_screen()
+            continue
+
+        #FIXME alignment
+        display.print_position(3,0,song_info("choices"))
+
         while True:
-            player_choice = input("What is your choice: ")
+            #FIXME
+            # Fails the readability of the code
+            player_choice = util.get_input_position(3,15)
             if player_choice.upper() in util.options(["A", "B", "C", "D", "H"]):
                 hint_used = False
-                #FIXME
                 if player_choice.upper() == "H":
                     if player_hints > 0:
                         if not hint_used:
-                            print(song_info("hint"))
+                            display.print_position(3,0,song_info("hint"))
                             hint_used = True
                             player_hints -= 1
                             while True:
-                                player_choice = input("What is your choice: ")
+                                player_choice = util.get_input_position(0,15)
                                 if player_choice.upper() in song_info("hint"):
                                     break
                                 else:
-                                    print("Invalid Option")
+                                    display.invalid_option(0,15)
 
+                #FIXME
                 if player_choice.upper() == (song_info("answer")):
                     print("Correct!")
 
@@ -68,6 +97,7 @@ def run_start_round(player_name):
 
         song_choice.pop(0)
         year_choice.pop(0)
+        player_skipped_song = False
 
         if util.list_is_empty(song_choice):
             player_choice = ask_play_again(player_name)
