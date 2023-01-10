419a18beb921b90c6d34770c26e604ffdef85643
umi <ianlumanog.iml@gmail.com>
2023-01-10 22:41:46 +0800

    Revert "Refactored the song selection"
    This reverts commit bbc28618902be17ac6aba00128dc80dc9730f662.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,30 @@
+import json
 from pygame import mixer
-from song_selection import run_song_selection
-from game_menu import run_game_menu
-from start_round import run_start_round
+from song_selection import song_selection
+from game_menu import start_game_menu
 import displays as display
 import utils as util
 
-player_name = None  # stores the player's name
+year_choice = []  # stores the year choice of player
+song_choice = []  # stores the song choice of player
+choice = {}  # stores the keys player can press and use it to call the key from the song list json file
+player_name = "mimi"  # stores the player's name
 player_choice = None  # stores th   e every action of the player.
 player_points = 0  # stores the points of the user once the round started
-
-def game_loop():
-    while True:
-        # run_game_menu(player_name)
-        run_song_selection()
-        run_start_round()
+# hints = 3
 
 def main():
     mixer.init()
     # start_game_menu(player_name)
     # play sound
-    # util.play_sound("sounds\\Song musics\\2020's\\Fallen - Lola Amour.wav")
-    display.set_screen_size(80,40)
+    util.play_sound("sounds\\Song musics\\2020's\\Fallen - Lola Amour.wav")
+    # displays.set_screen_size(80,40)
+    # display.set_screen_size(80,40)
     # display.loading_screen()
     # player_name = util.get_player_name()
     # display.advice_screen()
     # display.copyright_disclaimer_screen()
-    game_loop()
+    # song_selection(year_choice,song_choice,choice)
 
 
 if __name__ == "__main__":
