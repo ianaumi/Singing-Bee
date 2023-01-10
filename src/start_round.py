1f09b30fdb2ee483edf4a3fbcd150b39cf297655
umi <ianlumanog.iml@gmail.com>
2023-01-10 22:52:09 +0800

    Updated start round

--- src/start_round.py
+++ src/start_round.py
@@ -1,43 +1,45 @@
 import utils as util
 import displays as display
-import songList.json
-import main as mains
+from song_selection import song_list,choice,year_choice,song_choice
 
 
 def song_info(value):
-    return util.get_song_list_file[songList.year_choice[0]][songList.song_choice[0]][value]
+    return song_list[year_choice[0]][song_choice[0]][value]
 
 
 #FIXME parameter name
-def is_empty(empty):
-    if not empty:
+
+
+def ask_play_again(player_name):
+    print("[P] Play again")
+    print("Press any key to quit!")
+    player_choice = input(">>")
+    if player_choice.upper() == 'P':
         return True
     else:
-        return False
+        display.quit_screen(player_name)
 
+def run_start_round(player_name):
 
-def round_start():
-    global player_points
-    global hints
-    global player_choice
+    player_points = 0 # stores the points of the user once the round started
+    player_hints = 3
     while True:
-
-        print(songList.song_choice[0])
+        print(song_choice[0])
         print(song_info("lyrics"))
         print(song_info("choices"))
 
-        print("Total hints available: ", hints)
+        print("Total hints available: ", player_hints)
         while True:
             player_choice = input("What is your choice: ")
             if player_choice.upper() in util.options(["A", "B", "C", "D", "H"]):
                 hint_used = False
 
                 if player_choice.upper() == "H":
-                    if hints > 0:
+                    if player_hints > 0:
                         if not hint_used:
                             print(song_info("hint"))
                             hint_used = True
-                            hints = hints - 1
+                            player_hints -= 1
                             while True:
                                 player_choice = input("What is your choice: ")
                                 if player_choice.upper() in song_info("hint"):
@@ -63,16 +65,10 @@ def round_start():
             else:
                 print("Invalid Option")
 
-        songList.song_choice.pop(0)
-        songList.year_choice.pop(0)
+        song_choice.pop(0)
+        year_choice.pop(0)
 
-        if is_empty(songList.year_choice):
-            while True:
-                player_choice = input("Do you want to play again? Y/N : ")
-                if player_choice.upper() in util.options(["Y", "N"]):
-                    if player_choice.upper() == "Y":
-                        return mains.main()
-                    elif player_choice.upper() == "N":
-                        return display.quit_screen()
-                else:
-                    print("Invalid Option")
+        if util.list_is_empty(song_choice):
+            player_choice = ask_play_again(player_name)
+            if player_choice == True:
+                break
