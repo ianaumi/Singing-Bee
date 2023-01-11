2b8500768f9610b762bee115466fd80d46f6d36e
umi <ianlumanog.iml@gmail.com>
2023-01-12 01:59:18 +0800

    Fixed a lot of bugs

--- src/start_round.py
+++ src/start_round.py
@@ -18,25 +18,16 @@ def ask_play_again(player_name):
         display.quit_screen(player_name)
 
 def run_start_round(player_name):
-    # >>>>>>>>>>>>>>>>>>>>>TESTING BLOCK <<<<<<<<<<<<<<<<<<<<<
-    # while True:
-    #     print(song_list["2020s"]["The 1"]["lyrics"])
-    #     choice = input()
-    #     if choice == 'A':
-    #         exit()
     player_points = 0 # stores the points of the user once the round started
     player_hints = 3
     player_skipped_song = False
     player_used_hint = False
-
     while True:
-
         display.header_text(f"# Song playing - {song_choice[0]}\n\n ## Remaining songs: {len(song_choice)}")
 
         display.player_status(player_name,player_hints,player_points)
         # FIXME alignment -> assist w members
         display.print_position(2,0,song_info("lyrics"))
-
 
         if not player_skipped_song:
             pass
@@ -49,64 +40,64 @@ def run_start_round(player_name):
             # continue
 
         #FIXME alignment
-        #FIXME IF CONDITION WETHER TO PRINT CHOICES OR HINT CHOICES
-        display.print_position(2,15,song_info("choices"))
+        if player_used_hint :
+            display.print_position(3, 35, song_info("hint"))
+        else:
+            display.print_position(2, 15, song_info("choices"))
 
-        while True:
-            #FIXME
-            # Fails the readability of the code
-            player_choice = util.get_input_position(3,27)
-            if player_choice.upper() in util.options(["A", "B", "C", "D", "H"]):
-                player_used_hint = False
+        player_choice = util.get_input_position(3, 27)
 
-                if player_choice.upper() == "H":
-                    if player_hints > 0:
-                        if not player_used_hint:
-                            #FIXME CODE FROM THE TOP NOT PRINTING (LYRICS, CHOICES ETC.)
-                            display.print_position(3,35,song_info("hint"))
-                            player_used_hint = True
-                            player_hints = player_hints - 1
-                            while True:
-                                player_choice = util.get_input_position(0,27)
-                                if player_choice.upper() in song_info("hint"):
-                                    break
-                                else:
-                                    display.invalid_option(0,27)
+        # input validation
+        if player_choice.upper() in util.options(["A", "B", "C", "D", "H"]):
 
-                #FIXME WHERE TO PUT THE ANSWER CHECKINGS
-                #>>>>>>>>>> ANSWER CHECKER <<<<<<<<<<<<<<<<<<
-                if player_choice.upper() == (song_info("answer")):
-                    print("Correct!")
+            # checks if the player already used hint
+            if player_choice.upper() == 'H':
 
-                    #FIXME player used hint is still true after moving on to the next song
-                    if player_used_hint:
-                        print("You earned 500 honeys!")
-                        player_points = player_points + 500
-                        display.sleep(2)
-                        display.clear_screen()
-                        break
+                if player_used_hint:
+                    print("you already used hint")
+                    continue
 
-                    else:
-                        print("You earned 1000 honeys!")
-                        player_points = player_points + 1000
-                        display.sleep(2)
-                        display.clear_screen()
-                        break
+                # checks hint count and if player wants to use hint
+                if not player_used_hint and player_hints > 0:
+                    player_hints = player_hints - 1
+                    player_used_hint = True
+                    continue
+
                 else:
-                    print("Wrong")
-                    print("You earned 0 honeys")
-                    display.sleep(2)
-                    display.clear_screen()
-                    break
-            else:
-                display.invalid_option(0,27)
+                    print("you don't have enough hints")
+                    continue
 
-        song_choice.pop(0)
-        year_choice.pop(0)
+            # checks if user will enter an input aside from printed hint
+            if player_used_hint:
+                if player_choice.upper() not in song_info("hint"):
+                    print("invalid option")
+                    continue
+
+            # answer checker
+            elif player_choice.upper() == song_info("answer") :
+                if player_used_hint:
+                    player_points = player_points + 500
+                    continue
+
+                else:
+                    player_points = player_points + 1000
+            else:
+                print("wrong answer")
+        else:
+            display.invalid_option(0,0)
+            continue
+
+        player_used_hint = False
         player_skipped_song = False
 
+        # removes the last played song
+        song_choice.pop(0)
+        year_choice.pop(0)
+
+        # checks if there's no more song to play
         if util.list_is_empty(song_choice):
             display.total_score(player_name,player_points)
             player_choice = ask_play_again(player_name)
+            #FIXME
             if player_choice == True:
                 break
