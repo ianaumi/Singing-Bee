31ff5d2a366b1fe6473797790586994b39d47618
umi <ianlumanog.iml@gmail.com>
2023-01-12 12:56:44 +0800

    added test block

--- src/main.py
+++ src/main.py
@@ -10,21 +10,31 @@ player_choice = None  # stores the every action of the player.
 
 def game_loop():
     while True:
-        display.loading_screen()
-        run_game_menu(player_name)
+        # display.loading_screen()
+        # run_game_menu(player_name)
         run_song_selection()
         run_start_round(player_name)
 # hints = 3
 
 def main():
+    songlist = util.get_song_list_file()
     global player_name
     mixer.init()
     display.set_screen_size(80,40)
-    display.loading_screen()
-    player_name = util.get_player_name()
-    display.advice_screen()
-    display.copyright_disclaimer_screen()
-    game_loop()
+    # >>>>>>>>>>>>>>>>> TEST BLOCK <<<<<<<<<<<<<<<<<<<
+    # while True:
+    #     print(songlist["70s"]["War - Edwin Starr"]["lyrics"])
+    #     # middle line
+    #     print("-"*40)
+    #     choice = input()
+    #     if choice.upper() == 'A':
+    #         exit()
+
+    # display.loading_screen()
+    # player_name = util.get_player_name()
+    # display.advice_screen()
+    # display.copyright_disclaimer_screen()
+    # game_loop()
 
     # start_game_menu(player_name)
     # play sound
