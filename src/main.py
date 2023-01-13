8372553e4a5a36e4f296953928f79e75f5134465
umi <ianlumanog.iml@gmail.com>
2023-01-13 18:01:57 +0800

    Fixed: intro sound not looping

--- src/main.py
+++ src/main.py
@@ -41,7 +41,7 @@ def main():
     display.set_screen_size(80,40)
 
     # plays the intro sound theme
-    sounds.play_background("sounds\\Game sounds\\intro_background.wav",1)
+    sounds.play_background("sounds\\Game sounds\\intro_background.wav",-1)
     display.loading_screen()
 
     # gets the player name
