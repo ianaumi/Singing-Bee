3a343c731b5f655b4c1a86625c5e88acef64772e
umi <ianlumanog.iml@gmail.com>
2023-01-13 22:31:06 +0800

    Fixed: sound module bug

--- src/sounds.py
+++ src/sounds.py
@@ -1,16 +1,12 @@
-from pydub import AudioSegment
-from pydub.playback import play
 from pygame import mixer
+from playsound import playsound
 '''
 sounds file is used for playing sounds to the other files
 '''
 
 # plays a quick sound that overlaps pygame mixer
-
-
 def play_sound(path):
-    sound = AudioSegment.from_wav(path)
-    play(sound)
+    playsound(path)
 
 
 # plays a background music that can loop or not
