403b2b708bdc32329c034dd801d5991b8a7cddc4
umi <ianlumanog.iml@gmail.com>
2023-01-13 23:15:17 +0800

    Fixed: sound

--- src/start_round.py
+++ src/start_round.py
@@ -42,7 +42,7 @@ def run_start_round(player_name):
             sounds.stop_sound()
 
             # plays the background music while player is choosing answer
-            sounds.play_background("sounds\\Game sounds\\waiting_choice_sound.wav", -1)
+            sounds.play_background("sounds/Game sounds\\waiting_choice_sound.wav", -1)
             player_skipped_song = True
             display.clear_screen()
             continue
@@ -64,19 +64,19 @@ def run_start_round(player_name):
 
             # checks if player has enough hint
             if player_hint_count <= 0:
-                sounds.play_sound("sounds\\game_sounds\\invalid_sound.wav")
+                sounds.play_sound("sounds/game_sounds\\invalid_sound.wav")
                 display.hint_text_info("You dont have enough hints")
                 continue
 
             # checks if player already used a hint
             if player_used_hint:
-                sounds.play_sound("sounds\\game_sounds\\invalid_sound.wav")
+                sounds.play_sound("sounds/game_sounds\\invalid_sound.wav")
                 display.hint_text_info("You already used hint")
                 continue
 
             # checks player wants to use hint and has enough hint count
             if not player_used_hint and player_hint_count > 0:
-                sounds.play_sound("sounds\\game_sounds\\used_hint_sound.wav")
+                sounds.play_sound("sounds/game_sounds\\used_hint_sound.wav")
                 player_hint_count = player_hint_count - 1
                 player_used_hint = True
                 display.hint_text_info("You used hint")
@@ -87,25 +87,25 @@ def run_start_round(player_name):
 
             # checks if input is not in the hint choices
             if player_choice.upper() not in song_info("hint"):
-                sounds.play_sound("sounds\\game_sounds\\invalid_sound.wav")
+                sounds.play_sound("sounds/game_sounds\\invalid_sound.wav")
                 display.invalid_option(0, 30)
                 continue
 
         # >>>>>>>>> answer checker <<<<<<<<<<<<<<<<<
         # checks if it's the wrong answer
         if player_choice.upper() != song_info("answer"):
-            sounds.play_sound("sounds\\game_sounds\\wrong_answer_sound.wav")
+            sounds.play_sound("sounds/game_sounds\\wrong_answer_sound.wav")
             display.answer_result("Wrong answer.", 0)
 
         # checks if it's a correct answer and player used hint
         elif player_used_hint and player_choice.upper() == song_info("answer"):
-            sounds.play_sound("sounds\\game_sounds\\correct_with_hint_sound.wav")
+            sounds.play_sound("sounds/ame_sounds\\correct_with_hint_sound.wav")
             display.answer_result("Correct answer!", 500)
             player_points = player_points + 500
 
         # checks if it's a correct answer and player didn't use hint
         else:
-            sounds.play_sound("sounds\\game_sounds\\correct_answer_sound.wav")
+            sounds.play_sound("sounds/game_sounds\\correct_answer_sound.wav")
             display.answer_result("Correct answer!", 1000)
             player_points = player_points + 1000
 
