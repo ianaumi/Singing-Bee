601514855f34c2c287c03d98894590994e028f03
umi <ianlumanog.iml@gmail.com>
2023-01-14 11:27:26 +0800

    Removed: test block

--- src/main.py
+++ src/main.py
@@ -61,15 +61,6 @@ def main():
     # will proceed to game menu
     game_loop()
 
-    # >>>>>>>>>>>>>>>>> TEST BLOCK <<<<<<<<<<<<<<<<<<<
-    # while True:
-    #     print(songlist["2000s"]["With a Smile - Eraserheads"]["lyrics"])
-    #     # middle line
-    #     print("-"*40)
-    #     choice = input()
-    #     if choice.upper() == 'A':
-    #         exit()
-
 
 if __name__ == "__main__":
     main()
